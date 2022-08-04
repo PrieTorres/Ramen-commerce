a5fadf992fe3cf1c8f7e33c29ab26047e32a0ecf
Priscila Torres Benedito de Paula <priscila.torres@novamotores.com.br>
2022-08-04 17:16:11 -0300

    alterações compra js e css

--- js/seletorPagamento.js
+++ js/seletorPagamento.js
@@ -1,5 +1,12 @@
 function selecionarPagamento(pagamento){
+    document.body.querySelectorAll('.compra__selecao-pagamento__opcao').forEach(opcao => {
+        opcao.classList.remove('ativo');
+    });
+
+
     if(pagamento == 'online'){
         document.body.querySelector('.compra__selecao-pagamento__opcao.online').classList.add('ativo');
+    }else if(pagamento == 'entrega'){
+        document.body.querySelector('.compra__selecao-pagamento__opcao.entrega').classList.add('ativo');
     }
 }
