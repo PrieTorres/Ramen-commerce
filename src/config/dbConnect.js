c772631dfc03cdce9486e13a7d17ddf5836e6d8d
Priscila <priscilatorres2023@gmail.com>
2024-06-11 15:05:26 -0300

    fix: removing db connection

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -7,7 +7,9 @@ const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
 
 //mongoose.connect(`mongodb+srv://${user}:${password}@cluster0.ynfdedz.mongodb.net/test`);
-mongoose.connect(`mongodb+srv://user:user@principal.mwdxnup.mongodb.net/?retryWrites=true&w=majority`);
+// mongoose.connect(`mongodb+srv://user:user@principal.mwdxnup.mongodb.net/?retryWrites=true&w=majority`, err => {
+//  console.error(err);
+// });
 
 const db = mongoose.connection;
 
