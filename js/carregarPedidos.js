73f17a52e5e6bff7a828c89c8acf7a9d3b5dd9fa
Priscila <priscilatorres2023@gmail.com>
2024-08-14 16:13:39 -0300

    fix excluir pedido

--- js/carregarPedidos.js
+++ js/carregarPedidos.js
@@ -29,7 +29,7 @@ async function renderizarPedidos(pedidos, classeContainer) {
 
         container.innerHTML += `
         <div class="pedido-container">
-            <span class="delete-icon" onclick="excluirPedido('${pedido._id}')">X</span>
+            <span class="delete-icon" onclick="excluirPedido('${pedido._id ?? pedido.id}')">X</span>
             <div class="imagem-pedido">
                 <img src="${pedido.imagem ? pedido.imagem : './img/ramens/desenho-ramen.png'}" alt="ramen delicioso">
             </div>
@@ -48,8 +48,9 @@ async function renderizarPedidos(pedidos, classeContainer) {
 }
 
 function excluirPedido(pedidoId) {
+    if (!pedidoId) return;
     const localPedidos = JSON.parse(localStorage.getItem("pedidos")) ?? [];
-    const index = localPedidos.map(p => p.id).indexOf(pedidoId);
+    const index = localPedidos.map(p => p?.id ? `${p?.id}` : undefined).indexOf(pedidoId);
 
     if (index >= 0) {
         localPedidos.splice(index, 1);
